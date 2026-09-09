069e8d3d1bed73dc6140de1e32dbb4b21df56bb5
Nikhil Dikkatwar <itz.niks2008@gmail.com>
2026-09-09 13:10:36 +0530

    Change order amount for create-order API
    Updated order amount from 349900 to 500.

--- api/create-order.js
+++ api/create-order.js
@@ -34,7 +34,7 @@ module.exports = async (req, res) => {
     }
 
     const order = await razorpay.orders.create({
-      amount: 349900,
+      amount: 500,
       currency: "INR",
       receipt: `NS_${Date.now()}`,
       notes: {
